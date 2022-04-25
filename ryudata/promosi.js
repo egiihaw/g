bf3478f4d7a161a06ef1a395f9f72667ed65c12e
ZEXXO <102238551+ZEXOO@users.noreply.github.com>
2022-04-25 10:49:43 +0700

    Update promosi.js

--- ryudata/promosi.js
+++ ryudata/promosi.js
@@ -2,7 +2,7 @@ exports.promosi1 = () => {
 	return`Ready Vps/Rdp Murah
 
 Minat Chat
-wa.me/6285609233482
+wa.me/6289529291185
 `
 }
 
@@ -10,7 +10,7 @@ exports.promosi2 = () => {
 	return`Ready Lonte Murah
 
 Minat Chat
-wa.me/6285609233482
+wa.me/6289529291185
 `
 }
 
@@ -18,7 +18,7 @@ exports.promosi3 = () => {
 	return`Ready Cewe Murah
 
 Minat Chat
-wa.me/6285609233482
+wa.me/6289529291185
 `
 }
 
@@ -26,7 +26,7 @@ exports.promosi4 = () => {
 	return`Ready Akun Murah
 
 Minat Chat
-wa.me/6285609233482
+wa.me/6289529291185
 `
 }
 
@@ -34,6 +34,6 @@ exports.promosi5 = () => {
 	return`Ready Meki Murah
 
 Minat Chat
-wa.me/6285609233482
+wa.me/6289529291185
 `
 }
