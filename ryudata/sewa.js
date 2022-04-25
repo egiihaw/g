06acfa1580b4599ea4e8fbfd1966b44491072f6e
ZEXXO <102238551+ZEXOO@users.noreply.github.com>
2022-04-25 10:48:43 +0700

    Update sewa.js

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -3,9 +3,9 @@ exports.sewa = () => {
 
 ┏━━━━━━
 ┃𝗛𝗔𝗥𝗚𝗔 𝗦𝗘𝗪𝗔 :
-┃ *1 MINGGU : Rp 5.000*
-┃ *1 BULAN : Rp 15.000*
-┃ *PERMANEN : Rp 35.000*
+┃ *1 HARI : Rp 5.000*
+┃ *1 MINGGU : Rp 7.000*
+┃ *PERMANEN : Rp 10.000*
 ┗━━━━━━
 ┏━━━━━━
 ┃𝗙𝗜𝗧𝗨𝗥 𝗕𝗢𝗧 :
@@ -18,6 +18,6 @@ exports.sewa = () => {
 *SISTEM: TES BOT-MASUKIN BOT-TF-DONE*
 
 *MINAT? CHAT :
-wa.me/6285609233482
+wa.me/6289529291185
 `
 }
