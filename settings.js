e1434e4fc8352c5219866e55176de17e00cb0fac
ZEXXO <102238551+ZEXOO@users.noreply.github.com>
2022-04-25 10:41:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,64 +1,56 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-// Website Api
+//===> FREE API
 global.APIs = {
-	alfa: 'https://api-alphabot.herokuapp.com',
+	ryuu: 'https://api-ryubotz.herokuapp.com',
 }
 
-// Free apikey
 global.APIKeys = {
-	'https://api-alphabot.herokuapp.com': 'Alphabot',
+	'https://api-ryubotz.herokuapp.com': 'RyuBotz',
 }
 
-// setting 
-global.autoread = false // auto read pesan / message
-global.autorecording = true //status auto merekam ( auto record )
-global.autoketik = false //status auto mengetik (auto typing)
-global.available = false //status online (online)
-
-// Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZEXXO STORE ×፝֟͜×" //nama kalian
-global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.ownernomer = "6289529291185" // nomor wa kalian
-global.ownernomerr = "+6289529291185" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6289529291185","6289529291185","6289529291185"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© ZEXXO-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZEXXO STORE' //sticker wm ganti nama kalian
+//===> UBAH DISINI
+global.namaowner = 'ZEXXO STORE' //Ganti Jadi Namamu
+global.owner = ['6289529291185','6289529291185'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '6289529291185' //Owner Utama Istilahnya
+global.namabot = 'ZEXXOBOTZ MD' //Ganti Jadi Nama Botmu
+global.packname = '© ZEXXOBOTZ MD' //Sticker Weem
+global.author = 'ZEXXOBOTZ By ZEXXO STORE' //Sticker Weem
+global.webme = 'https://api-ryubotz.herokuapp.com'
 global.sessionName = 'session'
-global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.bg_welcome = 'https://telegra.ph/file/528b594d5d91c08482515.jpg' //Max 30kb Agar Tidak Delay
+global.youtube = 'https://youtube.com/c/RyuukaBotz'
+global.github = 'https://github.com/R-TEAM94'
+global.instagram = 'https://instagram.com/fandyyy._'
 
-global.prefa = ['','!','.','🐦','🐤','🗿']
+//===> BACKGROUND SETIAP MENU
+global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.bg2 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=monarch-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.bg3 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=yolo-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.bg4 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fun-run-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.prefa = ['','!','/','#','.']
 global.sp = '⭔'
-global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
-	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
-}
 
+//===> EDIT SESUAI KEMAUANMU
+global.mess = {
+admin: '*LU ADMIN? 🤨*',
+botAdmin: '*BOT HARUS MENJADI ADMIN 😔*',
+owner: '*LU BUKAN OWNER GW 😠*',
+group: '*KHUSUS GROUP 😏*',
+private: '*KHUSUS PRIVATE CHAT*',
+bot: '*KHUSUS PENGGUNA NOMER BOT*',
+wait: '*SABAR SEDANG DI PROSES ...*',
+done: '*DONE NGAB 🤗*',
+}
+global.thumb = fs.readFileSync('./media/ryuu.jpg')
+    
+//===> INI GAUSAH DIUBAH
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
 })
